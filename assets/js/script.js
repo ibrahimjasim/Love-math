5d5a994027fc89d09f1eadb8b3f23a1800bc0eb0
ibrahimjasim <>
2023-06-13 17:59:00 +0000

    Add structure of game function

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+function runGame(){
+
+
+}
+
+function checkAnswer(){
+
+
+}
+
+function calculateCorrectAnswer(){
+
+
+}
+
+function incremntScore(){
+
+
+}
+
+function incremntWrongAnswer(){
+
+
+}
+
+function displayAdditionQuestion(){
+
+
+}
+
+function displaySubtractQustion(){
+
+
+}
+
+function displayMultiplyQuestion(){
+
+}
+
