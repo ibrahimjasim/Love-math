3f320ccc6d8ea6d505fe0c465fafb9b28edcaa00
ibrahimjasim <>
2023-06-13 18:44:45 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,16 +8,26 @@ document.addEventListener("DOMContentLoaded", function () {
             } else {
                 let gameType = this.getAttribute("data-type");
                 alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition");
 });
 
 
-function runGame() {
+function runGame(gameType) {
 
     let num1 = math.floor(math.random()* 25 + 1)
     let num2 = math.floor(math.random() * 25 + 1);
+    
+    if(gameType==="addition"){
+        displayAdditionQuestion(num1, num2)
+    }else {
+        alart=("unnkown game type: ${gameType}");
+        throw "unnkown game type: ${gameType}.Aborting!";
+    }
 }
 
 
@@ -42,8 +52,11 @@ function incremntWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
 
+   document.getElementById("operand1").textContent = operand1
+    document.getElementById("operand2").textContent = operand2
+    document.getElementById("oprator").textContent = "+"
 
 }
 
